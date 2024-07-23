7ea47ee7c163173ddc0d6c956af66d6c040bfbaf
Shin Dong Won <eastorigin21@gmail.com>
2024-07-23 12:50:20 +0900

    9일차 강의 내용

---
+++ src/com/ktdsuniversity/edu/string/immutable/Student.java
@@ -0,0 +1,14 @@
+package com.ktdsuniversity.edu.string.immutable;
+
+public class Student {
+
+	private String name;
+	
+	public Student(String name) {
+		this.name = name;
+	}
+	
+	public void printName() {
+		System.out.println(this.name);
+	}
+}
